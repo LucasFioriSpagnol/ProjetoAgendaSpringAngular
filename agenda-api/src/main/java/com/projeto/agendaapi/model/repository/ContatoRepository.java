ace751bf156b11bbd2d1d4890ca9dacbb07c1ef4
Lucas Fiori <lucasiii.iii.o@gmail.com>
2022-05-09 14:56:50 -0300

    first commit

---
+++ agenda-api/src/main/java/com/projeto/agendaapi/model/repository/ContatoRepository.java
@@ -0,0 +1,7 @@
+package com.projeto.agendaapi.model.repository;
+
+import com.projeto.agendaapi.model.entity.Contato;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContatoRepository extends JpaRepository<Contato, Integer> {
+}
